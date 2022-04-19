04e9c1161bd96022046f19b619a46de3c4bbb173
nikkivacca <98358698+nikkivacca@users.noreply.github.com>
2022-04-19 16:49:07 -0500

    Add files via upload

--- Project.py
+++ Project.py
@@ -1,22 +1,25 @@
 '''
-Find a 'scrappable' cryptocurrencies website where 
-you can scrape the top 5 cryptocurrencies and 
-display as a formatted output one currency at a time. 
-The output should display the name of the currency, 
-the symbol (if applicable), the current price and 
-% change in the last 24 hrs and corresponding price 
-(based on % change)
+Find a 'scrappable' cryptocurrencies website where you can scrape the top 5 
+cryptocurrencies and display as a formatted output one currency at a time. 
+The output should display the name of the currency, the symbol (if applicable), 
+the current price and % change in the last 24 hrs and corresponding price (based on % change)
 
-Furthermore, for Bitcoin and Ethereum, 
-the program should alert you via text if the value 
+Furthermore, for Bitcoin and Ethereum, the program should alert you via text if the value 
 falls below $40,000 for BTC and $3,000 for ETH.
 
-Submit your GitHub URL which should contain all the 
-files worked in class as well as the above.
+Submit your GitHub URL which should contain all the files worked in class as well as the above.
 '''
-
+from twilio.rest import Client
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+
+
+accountSID = 'AC3dce134ef0d682e6ce6c44a3aba838cb'
+authToken = '8cbf2c5ee17ff7d8cf5b628037c3a062'
+client = Client(accountSID, authToken)
+TwilioNumber = '+12672140922'
+mycellphone = '+17327887622'
+
 
 webpage = 'https://coinmarketcap.com/'
 
@@ -35,14 +38,28 @@ for x in range (1,6):
     cols = rows[x].findAll('td')
     rank = x
     name = cols[1].text
-    current_price = cols[2].text
+    current_price = cols[2].text.replace(',', '').replace('$','')
+    current_price = float(current_price)
     change_percentage = cols[3].text
-   # old_price = 
+    change_float = float(change_percentage.text.strip('%'))
+    old_price = round(current_price / (1+ (change_float/100)), 2)
 
     print(f"Name: {name}")
     print(f"Rank : {rank}")
     print(f"Current Price: {current_price}")
     print(f"% Change in last 24 hours: {change_percentage}")
-   # print(f"Old Price: {old_price}")
+    print(f"Old Price: {old_price}")
+
+
+    if name == "Bitcoin":
+        btcprice = current_price
+        if btcprice < 40000:
+            textmessage = client.messages.create(to  = mycellphone, from_= TwilioNumber, 
+            body ='BTC is below $40,000')
+    if name == "Ethereum":
+        ethprice = current_price
+        if ethprice < 3000:
+            textmessage = client.messages.create(to  = mycellphone, from_= TwilioNumber, 
+            body ='ETH is below $3,000')
 
     input()
